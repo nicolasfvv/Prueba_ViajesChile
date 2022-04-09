c4afcd2457fadae9a8d31af121ae14ba57ff5bf1
Nicolas <nicolasfvv@gmail.com>
2022-04-09 12:56:49 -0400

    Comentario js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,7 @@
-$(document).ready(function () {
+/* $(document).ready(function () {
     $(document).scroll(function (e) {
         const y = $("html").scrollTop();
         if (y > 300) $("navbar").addClass("bg-dark")
         else $("navbar").removeClass("bg-dark")
     })
-});
+}); */
