b686dca18b8716ee00268abb63081f0631a38e86
Nicolas <nicolasfvv@gmail.com>
2022-04-10 19:54:55 -0400

    Viajes Chile terminada

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,28 @@
 $(document).ready(function () {
     $(window).scroll(function (e) {
-        const y = $("html").scrollTop();
-        if (y > 1200) {
+        const y = $("html").scrollTop()
+        if (y > 300) {
             $("nav").css("background-color", "#000")
         }
         else {
-            $("nav").css("background-color", "transparent" )
+            $("nav").css("background-color", "transparent")
         }
     })
+    $("#card_title1").click(function () {
+        $("#card_text1").toggle()
+    });
+    $("#card_title2").click(function () {
+        $("#card_text2").toggle()
+    });
+    $("#card_title3").click(function () {
+        $("#card_text3").toggle()
+    });
+    $("#card_title4").click(function () {
+        $("#card_text4").toggle()
+    });
 });
+
+var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
+var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
+    return new bootstrap.Tooltip(tooltipTriggerEl)
+})
